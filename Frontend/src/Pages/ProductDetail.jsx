b57594d30d965d3ce939b18b251258689177092d
ArpitNegi11 <arpit2802negi@gmail.com>
2025-07-31 14:09:36 +0530

    Update API endpoints to use production server URLs

--- Frontend/src/Pages/ProductDetail.jsx
+++ Frontend/src/Pages/ProductDetail.jsx
@@ -17,7 +17,7 @@ const ProductDetail = () => {
 
     const getProductDetail = async()=>{
 
-       await axios.get("http://localhost:3000/products/"+productId)
+       await axios.get("https://project-ecommerce-react-backend.onrender.com/"+productId)
         .then((res)=>{
             console.log(res);
             setProductData(res.data.product);
